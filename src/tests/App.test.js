7fc3dbe00d303416f92b38968e59e807efd89c39
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-03-22 15:48:09 -0300

    add tstes dos links - exercicio 1 ok

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,11 +1,31 @@
-import React from 'react'
-import { screen, render, fireEvent } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import App from '../App'
-import renderWithRouter from '../renderWithRouter'
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
-describe("Teste App.js", () => {
-    test("dasdasd", () => {
-        renderWithRouter(<App />)
-    })
-})
+describe('Teste App.js', () => {
+  test('Verifica Links da pagina', () => {
+    renderWithRouter(<App />);
+
+    const linkHome = screen.getByRole('link', { name: /home/i });
+    expect(linkHome).toBeDefined();
+
+    const linkAbout = screen.getByRole('link', { name: /about/i });
+    expect(linkAbout).toBeDefined();
+
+    const linkFavorites = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(linkFavorites).toBeDefined();
+  });
+
+  test('Verifica se ao clicar no link a pagina e redirecionada', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/home');
+    expect(history.location.pathname).toBe('/home');
+
+    history.push('/about');
+    expect(history.location.pathname).toBe('/about');
+
+    history.push('/favorites');
+    expect(history.location.pathname).toBe('/favorites');
+  });
+});
