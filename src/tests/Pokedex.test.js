a75250c8c39c595c4d3fdbd3d0da8837ab906fc5
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-03-22 18:15:41 -0300

    iniciando 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import Pokedex from '../components/Pokedex';
+
+describe('Testa a página Pokedex', () => {
+  it('Teste se página contém um heading h2 com o texto Encountered pokémons ', () => {
+    renderWithRouter(<Pokedex />);
+    const heading = screen.getByRole('heading',
+      { level: 2, name: /Encountered pokémons/i });
+    console.log(heading);
+    expect(heading).toBeInTheDocument();
+  });
+});
