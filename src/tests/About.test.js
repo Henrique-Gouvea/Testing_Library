8ef80a370be5ba207917cc65f668fe87c1f6f939
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-03-22 16:26:06 -0300

    criando About testes - exercicio 2 OK

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste About.js', () => {
+  test('Teste se a página contém as informações sobre a Pokédex.', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/about');
+
+    const informationPokedex = screen.getByText(/This application simulates a Pokédex/i);
+    expect(informationPokedex).toBeInTheDocument();
+
+    const headerPokedex = screen.getByText(/About Pokédex/i);
+    expect(headerPokedex).toBeInTheDocument();
+
+    const IMG_POKEDEX_SRC = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const imageAbout = screen.getByRole('img');
+    expect(imageAbout.src).toEqual(IMG_POKEDEX_SRC);
+  });
+});
